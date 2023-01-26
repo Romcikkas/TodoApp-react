49b06df4aea273eb78784283044cda91f4e1cc81
Romcikkas <romcikkas@gmail.com>
2023-01-26 16:19:44 +0200

    TodoApp

---
+++ src/components/Todos/Todo.js
@@ -0,0 +1,26 @@
+import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
+import styles from './Todo.module.css';
+
+function Todo({ todo, deleteTodo, toggleTodo }) {
+  return (
+    <div
+      className={`${styles.todo} ${
+        todo.isCompleted ? styles.completedTodo : ''
+      }`}
+    >
+      <RiTodoFill className={styles.todoIcon} />
+      <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line
+        className={styles.todoDelete}
+        onClick={() => deleteTodo(todo.id)}
+      />
+      <FaCheck
+        className={styles.todoComplete}
+        onClick={() => toggleTodo(todo.id)}
+      />
+    </div>
+  );
+}
+
+export default Todo;
